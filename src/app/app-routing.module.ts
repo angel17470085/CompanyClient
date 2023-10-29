bc81c9d566146e4a386bf607eaaddb75163307ee
angel17470085 <17470085@campeche.tecnm.mx>
2023-10-29 17:29:11 -0600

    Adding dummy router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,9 +6,16 @@ import { RegisterComponent } from './register/register.component';
 import { authorizeGuard } from './_guards/authorize.guard';
 
 const routes: Routes = [
+  {path:'', runGuardsAndResolvers:'always', canActivate:[authorizeGuard],
+    children:[
+      {path:'readCompanies', component: ReadCompaniesComponent},
+    ]
+  },
   {path:'register', component: RegisterComponent},
-  {path:'login', component: LoginComponent},
-  {path:'readCompanies', component: ReadCompaniesComponent, canActivate:[authorizeGuard]}];
+  {path:'login', component: LoginComponent}
+]
+  
+  
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
